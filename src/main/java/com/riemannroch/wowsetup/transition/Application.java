2f9c352a3d85ae197c572758319acc1e19d48b22
RiemannROch <dani221alves@gmail.com>
2022-03-15 17:34:54 -0300

    Initial commit

---
+++ src/main/java/com/riemannroch/wowsetup/transition/Application.java
@@ -0,0 +1,13 @@
+package com.riemannroch.wowsetup.transition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/riemannroch/wowsetup/transition/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riemannroch.wowsetup.transition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
